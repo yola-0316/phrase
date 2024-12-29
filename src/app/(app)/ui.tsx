1a2a2e0659e05353c06efc7faf1da0607c29de83
Zhi.Huang <zh.impact@outlook.com>
2024-12-30 03:12:34 +0800

    update: slogan

--- src/app/(app)/ui.tsx
+++ src/app/(app)/ui.tsx
@@ -60,7 +60,7 @@ export default function HomeUI() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="uppercase text-4xl sm:text-6xl">Phrase</h1>
+        <h1 className="text-4xl sm:text-6xl">一言一语</h1>
 
         <p>{hitokoto.hitokoto}</p>
 
