9f149ac513ab70b022f4784def69fdbfaf51d456
Zhi.Huang <zh.impact@outlook.com>
2024-12-30 15:31:34 +0800

    update: responsive ui

--- src/app/(app)/ui.tsx
+++ src/app/(app)/ui.tsx
@@ -111,7 +111,7 @@ export default function HomeUI() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-1/4">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start sm:w-1/2 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4">
         <Slogan />
 
         <p>{hitokoto?.hitokoto}</p>
